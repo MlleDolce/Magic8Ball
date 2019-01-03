d9fcaf113dc11a182a57958c477ab9fae63d3d1d
MlleDolce <andreakchristie@gmail.com>
2019-01-03 14:30:45 -0500

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Andrea Christie on 1/3/19.
+//  Copyright © 2019 Andrea Christie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
